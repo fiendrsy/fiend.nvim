344789df094083abe2f32b953161e2af65f35276
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-06-15 02:43:37 +0300

    feat(buffers): add checking for saves and duplicates

--- lua/fiend/buffers/buffer_picker.lua
+++ lua/fiend/buffers/buffer_picker.lua
@@ -3,6 +3,63 @@ local actions = require 'telescope.actions'
 
 local M = {}
 local api = vim.api
+local fn = vim.fn
+
+local function is_buffer_modified(bufnr)
+  return api.nvim_buf_get_option(bufnr, 'modified')
+end
+
+local function split_path(bufnr)
+  local path = api.nvim_buf_get_name(bufnr)
+
+  -- Extract file and dir for example: PATH.../dir/file.lua
+  local file = fn.fnamemodify(path, ':t')
+  local dir = fn.fnamemodify(path, ':h:t')
+
+  return {
+    file_name = file,
+    parent_dir = dir,
+    full_path = path,
+  }
+end
+
+local function get_display_name(entry, duplicates)
+  local MODIFIED_SYMBOL, SEPARATOR = '??', '/'
+
+  local path_fragments = split_path(entry.bufnr)
+  local display_name
+
+  if duplicates[path_fragments.file_name] then
+    display_name = path_fragments.parent_dir .. SEPARATOR .. path_fragments.file_name
+  else
+    display_name = path_fragments.file_name
+  end
+
+  if is_buffer_modified(entry.bufnr) then
+    display_name = MODIFIED_SYMBOL .. ' ' .. display_name
+  end
+
+  return display_name
+end
+
+local function find_duplicates(entries)
+  local counts = {}
+
+  for _, entry in ipairs(entries) do
+    local path_fragments = split_path(entry.bufnr)
+    counts[path_fragments.file_name] = (counts[path_fragments.file_name] or 0) + 1
+  end
+
+  local duplicates = {}
+
+  for name, count in pairs(counts) do
+    if count > 1 then
+      duplicates[name] = true
+    end
+  end
+
+  return duplicates
+end
 
 M.open_picker = function(opts)
   opts = opts or {}
@@ -19,12 +76,31 @@ M.open_picker = function(opts)
 
       if next(multi_selections) == nil then
         local selection = action_state.get_selected_entry()
+
+        if is_buffer_modified(selection.value) then
+          print 'Buffer is not saved! Save it before closing.'
+
+          actions.close(prompt_bufnr)
+
+          api.nvim_set_current_buf(selection.value)
+
+          return
+        end
+
         actions.close(prompt_bufnr)
-        vim.api.nvim_buf_delete(selection.value, { force = true })
+
+        api.nvim_buf_delete(selection.value, { force = true })
       else
         actions.close(prompt_bufnr)
+
         for _, selection in ipairs(multi_selections) do
-          vim.api.nvim_buf_delete(selection.value, { force = true })
+          if is_buffer_modified(selection.value) then
+            print 'One or more buffers are not saved! Save them before closing.'
+
+            return
+          end
+
+          api.nvim_buf_delete(selection.value, { force = true })
         end
       end
     end
@@ -45,17 +121,14 @@ M.open_picker = function(opts)
   end
 
   opts.entry_maker = function(entry)
-    local bufname = api.nvim_buf_get_name(entry.bufnr)
-
-    bufname = bufname:gsub('%%a', '')
-
-    local filename = vim.fn.fnamemodify(bufname, ':t')
-
     return {
       valid = true,
       value = entry.bufnr,
-      ordinal = filename,
-      display = filename,
+      ordinal = split_path(entry.bufnr).full_path,
+      display = function()
+        local duplicates = find_duplicates(fn.getbufinfo { buflisted = 1 })
+        return get_display_name(entry, duplicates)
+      end,
     }
   end
 
